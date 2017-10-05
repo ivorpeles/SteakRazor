5655326c871068fd5e53fafa4f1c26fcf61ac8d7
ivor.peles@mail.utoronto.ca <ivor.peles@mail.utoronto.ca>
2017-10-05 17:48:25 -0400

    add instructions to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-
+# python setup.py build_ext --inplace
 setup(ext_modules = cythonize(["dbs.py", "proc_hands.py", "os_tools.py", "main.py"]))
